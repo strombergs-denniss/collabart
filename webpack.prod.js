6b01a8cb41caafac60b0f3ca1a2c41e13f220dcc
alipinsh <a.lipinsh@gmail.com>
2022-09-25 17:06:42 +0300

    Production build

---
+++ webpack.prod.js
@@ -0,0 +1,41 @@
+const path = require('path')
+const Dotenv = require('dotenv-webpack')
+
+module.exports = {
+    mode: 'production',
+    entry: './source/index.js',
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [
+                    'babel-loader'
+                ]
+            },
+            {
+                test: /\.less$/,
+                exclude: /node_modules/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    {
+                        loader: 'less-loader',
+                        options: {
+                            lessOptions: {
+                                javascriptEnabled: true
+                            }
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new Dotenv()
+    ]
+}
